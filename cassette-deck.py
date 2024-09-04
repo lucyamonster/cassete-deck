d3f67a2f6085b4eb5fe58e4806e57683537af634
lucyamonster <m0nster.lucy2023@gmail.com>
2024-09-04 09:11:39 +0200

    data preping done

--- cassette-deck.py
+++ cassette-deck.py
@@ -9,7 +9,7 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
-GAME_FONT = pygame.freetype.Font("NerkoOne-Regular.ttf", 24)
+GAME_FONT = pygame.freetype.Font("Base-Font/NerkoOne-Regular.ttf", 24)
 
 while running:
     # poll for events
@@ -19,15 +19,38 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("green")
 
     # RENDER YOUR GAME HERE
     a = subprocess.run(['vlc-ctrl',  'info'], stdout=subprocess.PIPE)
     metadata = a.stdout
 
-    print(metadata)
-    
-    GAME_FONT.render_to(screen, (100, 100), str(metadata), (0, 0, 0))
+    print("-"*10)
+    if str(metadata) == "b''":
+        print("error, no vlc data")
+        GAME_FONT.render_to(screen, (100, 100), "No vlc out found :(", (0, 0, 0))
+    else:
+        print(metadata)
+
+        x = str(metadata).split("\\n")
+
+        print(x)
+        print("-"*5)
+        print(str(x[0])[2:])
+        print(x[1])
+        print(x[2])
+        print(x[5])
+
+        album = str(str(x[0])[2:])
+        title = str(x[1])
+        artist = str(x[2])
+        genre = str(x[5])
+        x = 0
+        y = 0
+        GAME_FONT.render_to(screen, (x, 0+y), str(album), (0, 0, 0))
+        GAME_FONT.render_to(screen, (x, 30+y), str(title), (0, 0, 0))
+        GAME_FONT.render_to(screen, (x, 60+y), str(artist), (0, 0, 0))
+        GAME_FONT.render_to(screen, (x, 90+y), str(genre), (0, 0, 0))
 
 
     # flip() the display to put your work on screen
