11249d9db8cb0c7735dda3bdea40fb4cfc8ea6b0
lucyamonster <m0nster.lucy2023@gmail.com>
2024-09-04 09:44:39 +0200

    cleaning up

--- cassette-deck.py
+++ cassette-deck.py
@@ -7,14 +7,34 @@ import pygame
 pygame.init()
 clock = pygame.time.Clock()
 running = True
+
+x = 0
+y = 0
+clockrate = 5
 scale = 64
-screen = pygame.display.set_mode((scale * 16, scale * 5))
-
-
 fontsize = scale
+
+screen = pygame.display.set_mode((scale * 16, scale * 5))
 
 GAME_FONT = pygame.freetype.Font("Base-Font/NerkoOne-Regular.ttf", fontsize)
 fontsize += (fontsize//4)
+
+def slice_and_print():
+    # Split by line
+    splitdata = str(metadata).split("\\n")
+
+    # Prep and assign lines
+    album = str(str(splitdata[0])[2:])
+    title = str(splitdata[1])
+    artist = str(splitdata[2])
+    genre = str(splitdata[5])
+
+    # Print to window
+    GAME_FONT.render_to(screen, (x, (fontsize*0)+y), str(album), (0, 0, 0))
+    GAME_FONT.render_to(screen, (x, (fontsize*1)+y), str(title), (0, 0, 0))
+    GAME_FONT.render_to(screen, (x, (fontsize*2)+y), str(artist), (0, 0, 0))
+    GAME_FONT.render_to(screen, (x, (fontsize*3)+y), str(genre), (0, 0, 0))
+
 
 while running:
     # poll for events
@@ -30,37 +50,14 @@ while running:
     a = subprocess.run(['vlc-ctrl',  'info'], stdout=subprocess.PIPE)
     metadata = a.stdout
 
-    print("-"*10)
     if str(metadata) == "b''":
-        print("error, no vlc data")
         GAME_FONT.render_to(screen, (100, 100), "No vlc out found :(", (0, 0, 0))
     else:
-        print(metadata)
-
-        x = str(metadata).split("\\n")
-
-        print(x)
-        print("-"*5)
-        print(str(x[0])[2:])
-        print(x[1])
-        print(x[2])
-        print(x[5])
-
-        album = str(str(x[0])[2:])
-        title = str(x[1])
-        artist = str(x[2])
-        genre = str(x[5])
-        x = 0
-        y = 0
-        GAME_FONT.render_to(screen, (x, (fontsize*0)+y), str(album), (0, 0, 0))
-        GAME_FONT.render_to(screen, (x, (fontsize*1)+y), str(title), (0, 0, 0))
-        GAME_FONT.render_to(screen, (x, (fontsize*2)+y), str(artist), (0, 0, 0))
-        GAME_FONT.render_to(screen, (x, (fontsize*3)+y), str(genre), (0, 0, 0))
-
+        slice_and_print()
 
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(5)  # limits FPS to 60
+    clock.tick(clockrate)  # limits FPS to 60
 
 pygame.quit()
