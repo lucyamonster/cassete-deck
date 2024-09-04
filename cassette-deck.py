7dbc53edb6603da64551891b10157e2905729fd3
lucyamonster <m0nster.lucy2023@gmail.com>
2024-09-04 10:51:15 +0200

    Adding basic icon

--- cassette-deck.py
+++ cassette-deck.py
@@ -5,19 +5,39 @@ import pygame
 
 # pygame setup
 pygame.init()
+pygame.display.set_caption("Cassette Deck")
 clock = pygame.time.Clock()
 running = True
+tick = 0
 
 x = 0
 y = 0
-clockrate = 5
+
+x_text = x
+y_text = y
+
+x_cas = x + 10
+y_cas = y - 20
+
+clockrate = 20
 scale = 64
 fontsize = scale
 
-screen = pygame.display.set_mode((scale * 16, scale * 5))
+cas_rotaition_limit = 25
+
+just_text_mode = False
+
+if just_text_mode:
+    screen = pygame.display.set_mode((scale * 16, scale * 5))
+else:
+    screen = pygame.display.set_mode(((scale * 16)*1, (scale * 5)*1.8))
+    y_text += 128 * (scale//31)
 
 GAME_FONT = pygame.freetype.Font("Base-Font/NerkoOne-Regular.ttf", fontsize)
+cassette_img = pygame.image.load("Icons/Cassette 128x128.png")
 fontsize += (fontsize//4)
+
+updatedisplaysize = False
 
 def slice_and_print():
     # Split by line
@@ -30,10 +50,10 @@ def slice_and_print():
     genre = str(splitdata[5])
 
     # Print to window
-    GAME_FONT.render_to(screen, (x, (fontsize*0)+y), str(album), (0, 0, 0))
-    GAME_FONT.render_to(screen, (x, (fontsize*1)+y), str(title), (0, 0, 0))
-    GAME_FONT.render_to(screen, (x, (fontsize*2)+y), str(artist), (0, 0, 0))
-    GAME_FONT.render_to(screen, (x, (fontsize*3)+y), str(genre), (0, 0, 0))
+    GAME_FONT.render_to(screen, (x_text, (fontsize*0)+y_text), str(album), (0, 0, 0))
+    GAME_FONT.render_to(screen, (x_text, (fontsize*1)+y_text), str(title), (0, 0, 0))
+    GAME_FONT.render_to(screen, (x_text, (fontsize*2)+y_text), str(artist), (0, 0, 0))
+    GAME_FONT.render_to(screen, (x_text, (fontsize*3)+y_text), str(genre), (0, 0, 0))
 
 
 while running:
@@ -42,6 +62,8 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    tick += 1
+    print(tick )
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill((255,0,255,0))
@@ -53,10 +75,16 @@ while running:
     if str(metadata) == "b''":
         GAME_FONT.render_to(screen, (100, 100), "No vlc out found :(", (0, 0, 0))
     else:
+        animated_cassette = pygame.transform.rotozoom(cassette_img, (tick % cas_rotaition_limit) - (cas_rotaition_limit // 2), scale//17)
+        screen.blit(animated_cassette, (x_cas, y_cas))
         slice_and_print()
 
     # flip() the display to put your work on screen
     pygame.display.flip()
+
+    if updatedisplaysize:
+        screen = pygame.display.set_mode((scale * 16, scale * 5))
+        updatedisplaysize = False
 
     clock.tick(clockrate)  # limits FPS to 60
 
